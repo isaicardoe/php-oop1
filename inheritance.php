4df534e2fae3cc8d6e2147be9e91a173b32f2f7f
isai cardoso <isai.cardoe@gmail.com>
2015-01-13 12:28:23 -0800

    starting to make the class

--- inheritance.php
+++ inheritance.php
@@ -1,8 +1,12 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+   
+  class Player{
+      public $cleats;
+      public $shingards;
+      public $hairstyle;
+      public $shirtnum;
+      public $gender;
+      public $position;
+      
+      
+  }
