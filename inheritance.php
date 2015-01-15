e3afa2db8fae8744007449812c6de05b0bcc811a
isai cardoso <isai.cardoe@gmail.com>
2015-01-15 12:06:06 -0800

    lesson 5

--- inheritance.php
+++ inheritance.php
@@ -1,19 +1,30 @@
 <?php
    
-  class Player{
+  class Player
+{
       public $cleats;
       public $shingards;
       public $hairstyle;
       public $shirtnum;
       public $position;
        
-        function__construct($cleats, $shingards, $hairstyle, $shirtnum, $position){
+      function __construct($cleats, $shingards, $hairstyle, $shirtnum, $position)
+       {
             $this->cleats = $cleats;
             $this->shingards = $shingards;
             $this->hairstyle = $hairstyle;
             $this->shirtnum = $shirtnum;
             $this->position = $position;
         }
-      
-      
-  }
+        function getName()
+        {
+            return "cleats: " . $this->cleats . "<br />" .
+                   "shingards: " . $this->shingards . "<br />" .
+                   "hairstyle: " . $this->hairstyle . "<br />" .
+                   "shirtnum: " . $this->shirtnum . "<br />" .
+                   "position: " . $this->position;
+        }     
+}
+    $messi = new Player("messif50", "addidas", "messy", "10", "center forward");
+    echo $messi->getName();
+ 
